92d359a73c10207d7f19a73fb2c4d9e2a3aa8931
evandrohvaz <evandrohvaz@gmail.com>
2023-10-15 15:23:31 -0300

    first commit

---
+++ src/Iphone.java
@@ -0,0 +1,48 @@
+public class Iphone implements AparelhoTelefonico, ReprodutorMusical, NavegadornaInternet{
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo ligação!");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo página.");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando a pagina...");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Telefone tocando!");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecionando música!");
+    }
+
+}
---
+++ src/NavegadornaInternet.java
@@ -0,0 +1,5 @@
+public interface NavegadornaInternet {
+    public void exibirPagina();
+    public void adicionarNovaAba();
+    public void atualizarPagina();
+}
